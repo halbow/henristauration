d6eac703dd31480215a974ca614865a65c274ee5
Halbow <alexis_huberlant@hotmail.com>
2019-06-01 17:37:19 +0200

    Added frontend mail logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,7 +30,9 @@ import { MenuContentComponent } from './menu-content/menu-content.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,18 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Mail } from '../mail';
+import { MailService } from '../mail.service';
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  name: string;
+  email: string;
+  message: string;
 
-  constructor() { }
+  constructor(private mailService: MailService) { }
 
   ngOnInit() {
   }
 
   onSubmit(content) {
-    alert('mail');
+    const allInfo = `My name is ${this.name}. My email is ${this.email}. My message is ${this.message}`;
+    this.mailService.send(this.name, this.email, this.message).subscribe(
+      res => {
+        alert(res['data']);
+      }
+    );
   }
 }
