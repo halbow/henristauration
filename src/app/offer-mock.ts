ad36272e31cb861f1dd2f116ea84201918a1aba0
Halbow <alexis_huberlant@hotmail.com>
2019-09-14 09:53:23 +0200

    old refactoring

--- src/app/offer-mock.ts
+++ src/app/offer-mock.ts
@@ -1,28 +1,30 @@
 import { Offer } from "./offer";
+
+const ASSETS = "./assets/images/";
 export const OFFERS: Offer[] = [
     /* breakfast */
     {
         name: "Formule petit-déjeuner 1",
-        price: 2.99,
+        price: 333.99,
         type: "breakfast",
-        thumb: "../assets/images/formules/pd-f1-small.jpg",
-        src: "../assets/images/formules/pd-f1.jpg",
+        thumb: ASSETS.concat("formules / pd - f1 - small.jpg"),
+        src: ASSETS.concat("formules / pd - f1.jpg"),
         caption: "1 Pains au chocolat + jus/café"
     },
     {
         name: "Formule petit-déjeuner 2",
         price: 3.99,
         type: "breakfast",
-        thumb: "../assets/images/formules/pd-f2-small.jpg",
-        src: "../assets/images/formules/pd-f2.jpg",
+        thumb: ASSETS.concat("formules/pd-f2-small.jpg"),
+        src: ASSETS.concat("formules/pd-f2.jpg"),
         caption: "1 Pains au chocolat + 1 croissant + jus/café"
     },
     {
         name: "Formule petit-déjeuner 3",
         price: 4.99,
         type: "breakfast",
-        thumb: "../assets/images/formules/pd-f3-small.jpg",
-        src: "../assets/images/formules/pd-f3.jpg",
+        thumb: ASSETS.concat("formules/pd-f3-small.jpg"),
+        src: ASSETS.concat("ormules/pd-f3.jpg"),
         caption: "1 Pains au chocolat + 1 croissant + jus/café + fruits"
     },
     /* wdiner */
@@ -30,8 +32,8 @@ export const OFFERS: Offer[] = [
         name: "Formule 1",
         price: 7.99,
         type: "wdiner",
-        thumb: "../assets/images/formules/pd-f3-small.jpg",
-        src: "../assets/images/formules/pd-f3.jpg",
+        thumb: ASSETS.concat("./assets/images/formules/pd-f3-small.jpg"),
+        src: ASSETS.concat("formules/pd-f3.jpg"),
         caption: "1 Pains au chocolat + 1 croissant + jus/café + fruits"
     },
     /* bread */
@@ -39,8 +41,8 @@ export const OFFERS: Offer[] = [
         name: "Formule 1",
         price: 7.99,
         type: "bread",
-        thumb: "../assets/images/formules/pd-f3-small.jpg",
-        src: "../assets/images/formules/pd-f3.jpg",
+        thumb: ASSETS.concat("formules/pd-f3-small.jpg"),
+        src: ASSETS.concat("formules/pd-f3.jpg"),
         caption: "1 Pains au chocolat + 1 croissant + jus/café + fruits"
     },
     /* diner */
@@ -48,8 +50,8 @@ export const OFFERS: Offer[] = [
         name: "Formule 1",
         price: 11.99,
         type: "diner",
-        thumb: "../assets/images/formules/pd-f3-small.jpg",
-        src: "../assets/images/formules/pd-f3.jpg",
+        thumb: ASSETS.concat("formules/pd-f3-small.jpg"),
+        src: ASSETS.concat("formules/pd-f3.jpg"),
         caption: "1 Pains au chocolat \n 1 croissant + jus/café + fruits"
     },
     /* porchetta */
@@ -57,8 +59,8 @@ export const OFFERS: Offer[] = [
         name: "Formule 1",
         price: 10.99,
         type: "porchetta",
-        thumb: "../assets/images/formules/pork-small.jpg",
-        src: "../assets/images/formules/pork.jpg",
+        thumb: ASSETS.concat("formules/pork-small.jpg"),
+        src: ASSETS.concat("formules/pork.jpg"),
         caption: "1 Pains au chocolat + 1 croissant + jus/café + fruits"
     },
     /* bbq */
@@ -66,8 +68,8 @@ export const OFFERS: Offer[] = [
         name: "Formule 1",
         price: 12.99,
         type: "bbq",
-        thumb: "../assets/images/formules/pd-f3-small.jpg",
-        src: "../assets/images/formules/pd-f3.jpg",
+        thumb: ASSETS.concat("formules/pd-f3-small.jpg"),
+        src: ASSETS.concat("formules/pd-f3.jpg"),
         caption: "1 Pains au chocolat + 1 croissant + jus/café + fruits"
     },
     /* Menu */
@@ -75,8 +77,8 @@ export const OFFERS: Offer[] = [
         name: "Formule 1",
         price: 15.99,
         type: "menu",
-        thumb: "../assets/images/formules/pd-f3-small.jpg",
-        src: "../assets/images/formules/pd-f3.jpg",
+        thumb: ASSETS.concat("formules/pd-f3-small.jpg"),
+        src: ASSETS.concat("formules/pd-f3.jpg"),
         caption: "1 Pains au chocolat + 1 croissant + jus/café + fruits"
     },
 ]
